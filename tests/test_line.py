8cdf40eadb31081343944973053f455b464f79e6
ZhangWenxiang <1007201991@qq.com>
2020-01-07 09:44:04 +0800

    typo

--- tests/test_line.py
+++ tests/test_line.py
@@ -5,7 +5,7 @@ sys.path.append('../')
 
 def test_line():
 
-    os.system("/Users/zhangwenxiang1/anaconda3/envs/tensorflow/bin/python ../scripts/train.py --task unsupervised_node_classification --dataset wikipedia --model line --seed 0 1 2 3 4")
+    os.system("python ../scripts/train.py --task unsupervised_node_classification --dataset wikipedia --model line --seed 0 1 2 3 4")
     pass
 
 if __name__ == "__main__":
